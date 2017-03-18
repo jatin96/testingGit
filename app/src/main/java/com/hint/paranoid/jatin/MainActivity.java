85071c80b5d24f050649c9c1dcabb8bb02960b6a
jatin_jt_narula <jatin.jt.narula@gmail.com>
2017-03-18 16:35:40 +0530

    sample commit first

---
+++ app/src/main/java/com/hint/paranoid/jatin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hint.paranoid.jatin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
